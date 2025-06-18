dfe79dad72988feefbb9a1ee535b8e19e4ce66bf
Luiz Chaves <luizchaves97@gmail.com>
2025-06-18 01:02:55 -0300

    feat: using args production instead of node_env

--- remote/webpack.config.js
+++ remote/webpack.config.js
@@ -3,50 +3,50 @@ const { withZephyr } = require("zephyr-webpack-plugin");
 const { ModuleFederationPlugin } = require('webpack').container;
 const path = require('path');
 
-const MODE = process.env.NODE_ENV || "development";
-const IS_PRODUCTION = MODE === "production";
-
-const config = {
-  entry: './src/index',
-  mode: MODE,
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: 3002,
-    hot: false,
-    liveReload: true,
-    watchFiles: ['src/**/*', 'public/**/*'],
-  },
-  output: {
-    publicPath: 'auto',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            ["@babel/preset-react", { runtime: "automatic", development: !IS_PRODUCTION }]
-          ],
+module.exports = (_, argv) => {
+  const MODE = argv.mode || 'development';
+  const IS_PRODUCTION = MODE === 'production';
+  const config = {
+    entry: './src/index',
+    mode: MODE,
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      port: 3002,
+      hot: false,
+      liveReload: true,
+      watchFiles: ['src/**/*', 'public/**/*'],
+    },
+    output: {
+      publicPath: 'auto',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          options: {
+            presets: [
+              ["@babel/preset-react", { runtime: "automatic", development: !IS_PRODUCTION }]
+            ],
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      // To learn more about the usage of this plugin, please visit https://webpack.js.org/plugins/module-federation-plugin/
+      new ModuleFederationPlugin({
+        name: 'remoteApp',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './App': './src/App',
+        },
+        shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
     ],
-  },
-  plugins: [
-    // To learn more about the usage of this plugin, please visit https://webpack.js.org/plugins/module-federation-plugin/
-    new ModuleFederationPlugin({
-      name: 'remoteApp',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App',
-      },
-      shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
+  };
+  return IS_PRODUCTION ? withZephyr()(config) : config;
 };
-
-module.exports = IS_PRODUCTION ? withZephyr()(config) : config;
--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -4,46 +4,47 @@ const {ModuleFederationPlugin} = require("webpack").container;
 const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
 const path = require("path");
 
-const MODE = process.env.NODE_ENV || "development";
-const IS_PRODUCTION = MODE === "production";
+module.exports = (_, argv) => {
+  const MODE = argv.mode || "development";
+  const IS_PRODUCTION = MODE === "production";
 
-const config = {
-  entry: "./src/index",
-  mode: MODE,
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    port: 3001,
-  },
-  output: {
-    publicPath: "auto",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            ["@babel/preset-react", { runtime: "automatic", development: !IS_PRODUCTION }]
-          ],
+  const config = {
+    entry: "./src/index",
+    mode: MODE,
+    devServer: {
+      static: path.join(__dirname, "dist"),
+      port: 3001,
+    },
+    output: {
+      publicPath: "auto",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: "babel-loader",
+          exclude: /node_modules/,
+          options: {
+            presets: [
+              ["@babel/preset-react", { runtime: "automatic", development: !IS_PRODUCTION }]
+            ],
+          },
         },
-      },
+      ],
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "shellApp",
+        remotes: {
+          remoteApp: "remoteApp@[remoteAppUrl]/remoteEntry.js",
+        },
+        shared: {react: {singleton: true}, "react-dom": {singleton: true}},
+      }),
+      new ExternalTemplateRemotesPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
     ],
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "shellApp",
-      remotes: {
-        remoteApp: "remoteApp@[remoteAppUrl]/remoteEntry.js",
-      },
-      shared: {react: {singleton: true}, "react-dom": {singleton: true}},
-    }),
-    new ExternalTemplateRemotesPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-  ],
+  };
+  return IS_PRODUCTION ? withZephyr()(config) : config;
 };
-
-module.exports = IS_PRODUCTION ? withZephyr()(config) : config;
